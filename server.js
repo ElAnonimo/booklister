0991a8cbd966b780a7a0cc560238afbf98f15017
elanonimo <mgrbmk@emailhosting.com>
2018-12-29 15:08:01 +0300

    added readme

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use('/api/books', books);
 // serve static assets when in production
 if (process.env.NODE_ENV === 'production') {
 	// set static folder
-	app.use(express.static('client/build'));
+	app.use(express.static('client/dist'));
 	app.get('*', (req, res) => {
 		res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'));
 	});
